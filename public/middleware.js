e3b0e1b317b6580a6e2c8cb15c19db0c1ed966f1
kzlefevre <kevin.lefevre@gmail.com>
2017-01-21 11:01:29 -0700

    satmorn

--- public/middleware.js
+++ public/middleware.js
@@ -1,25 +1,25 @@
-// var User = require('./models/users')
-//
-// module.exports = {
-//   isLoggedIn : (req, res, next) =>{
-//     if(req.session.userID){
-//       next()
-//     }
-//     else{
-//       res.status(403).send('You shall not pass!');
-//     }
-//   },
-//
-//   isAdmin : (req, res, next) =>{
-//     User.findOne({_id : req.session.userID}, (err, user)=>{
-//
-//       if(user && (user.role > 1)){
-//         next()
-//       }
-//       else{
-//         res.status(403).send('You shall not pass!');
-//       }
-//
-//     })
-//   }
-// }
+var User = require('./models/users')
+
+module.exports = {
+  isLoggedIn : (req, res, next) =>{
+    if(req.session.userID){
+      next()
+    }
+    else{
+      res.status(403).send('You shall not pass!');
+    }
+  },
+
+  isAdmin : (req, res, next) =>{
+    User.findOne({_id : req.session.userID}, (err, user)=>{
+
+      if(user && (user.role > 1)){
+        next()
+      }
+      else{
+        res.status(403).send('You shall not pass!');
+      }
+
+    })
+  }
+}
--- public/routes.js
+++ public/routes.js
@@ -1,1 +1,33 @@
+var Users = require('./controllers/users');
+var Exhibits = require('./controllers/exhibits');
+var User = require('./models/users');
 var Middleware = require('./middleware');
+
+
+
+module.exports = (app) => {
+  app.get('/', (req, res)=>{
+    res.sendFile('index.html', {root : './public/html'});
+  });
+
+  app.get('/api/me', (req, res)=>{
+//     res.send(req.session.userID); // send down their ID
+
+    User.findOne({_id : req.session.userID}, (err, user) =>{
+      res.send(user) // send down their object
+    })
+
+  })
+
+  app.get('/logout', (req, res)=>{
+    req.session.reset(); // Destroys user's session
+    res.redirect('/')
+  });
+
+  app.get('/api/users', Middleware.isLoggedIn, Users.get);
+  app.post('/api/users', Users.create);
+  app.post('/api/users/login', Users.login);
+
+  app.get('/api/exhibits', Middleware.isLoggedIn, Middleware.isAdmin, Exhibits.get);
+
+}
